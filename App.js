d69126f4bdea266c49b17f8263b4593200dda265
Aamer2198 <125115335+Aamer2198@users.noreply.github.com>
2023-12-16 13:26:12 +0530

    Files Added

---
+++ App.js
@@ -0,0 +1,44 @@
+import { Navbar } from './components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Shop } from './pages/Shop'
+import { ShopCategory } from './pages/ShopCategory'
+import { Product } from './pages/Product'
+import { Cart } from './pages/Cart'
+import { LoginSignUp } from './pages/LoginSignUp'
+import { Footer } from './components/Footer/Footer'
+import men_banners from './components/Assets/banner_mens.png'
+import women_banner from './components/Assets/banner_women.png'
+import kid_banner from './components/Assets/banner_kids.png'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banners} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
